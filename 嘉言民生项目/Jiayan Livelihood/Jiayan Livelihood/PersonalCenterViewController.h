10837d0bd0cac116eaddc89cb1d33a2bce96c0bc
jiayanminsheng <hanlu@bjgh.org>
2016-01-21 17:40:09 +0800

    重新布局个人中心

--- 嘉言民生项目/Jiayan Livelihood/Jiayan Livelihood/PersonalCenterViewController.h
+++ 嘉言民生项目/Jiayan Livelihood/Jiayan Livelihood/PersonalCenterViewController.h
@@ -1,13 +1,18 @@
 //
 //  PersonalCenterViewController.h
-//  Jiayan Livelihood
+//  01demo
 //
-//  Created by bqm on 16/1/5.
+//  Created by bqm on 16/1/21.
 //  Copyright © 2016年 bqm. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface PersonalCenterViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *headerBtn;
+
+@property (weak, nonatomic) IBOutlet UILabel *userNick;
+
+
 
 @end
