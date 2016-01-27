683e2c80d389a3402d8e323dd91c203e85a2d1f6
jiayanminsheng <hanlu@bjgh.org>
2016-01-27 14:20:43 +0800

    下拉菜单功能代码实现   产品发布

--- jyms_bqm/Jiayan Livelihood/Jiayan Livelihood/Classes/Business/Controllers/ProductViewController.h
+++ jyms_bqm/Jiayan Livelihood/Jiayan Livelihood/Classes/Business/Controllers/ProductViewController.h
@@ -9,5 +9,29 @@
 #import <UIKit/UIKit.h>
 
 @interface ProductViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+@property (weak, nonatomic) IBOutlet UILabel *typeNameLabel1;
+@property (weak, nonatomic) IBOutlet UILabel *typeNameLabel2;
+@property (weak, nonatomic) IBOutlet UITextField *num_unitTextField;
+@property (weak, nonatomic) IBOutlet UILabel *num_unitLabel;
+@property (weak, nonatomic) IBOutlet UITextField *price_unitTextField;
+@property (weak, nonatomic) IBOutlet UILabel *price_unitLabel;
+@property (weak, nonatomic) IBOutlet UITextField *addressTextField;
+@property (weak, nonatomic) IBOutlet UITextField *contactNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *contactPhoneTextField;
+@property (weak, nonatomic) IBOutlet UILabel *placeLabel;
+@property (weak, nonatomic) IBOutlet UITextView *describeTextView;
+@property (weak, nonatomic) IBOutlet UIScrollView *imageScrollView;
+@property (weak, nonatomic) IBOutlet UIButton *agreeButton;
+@property (weak, nonatomic) IBOutlet UIButton *agreeReleseButton;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *typeNameOneBtn;
+@property (weak, nonatomic) IBOutlet UIButton *typeNameTwoBtn;
+@property (weak, nonatomic) IBOutlet UIButton *num_unitBtn;
+@property (weak, nonatomic) IBOutlet UIButton *price_unitBtn;
+
+@property (weak, nonatomic) IBOutlet UIButton *middleBtn;
+
 
 @end
--- jyms_bqm/Jiayan Livelihood/Jiayan Livelihood/Classes/Business/Controllers/ProductViewController.m
+++ jyms_bqm/Jiayan Livelihood/Jiayan Livelihood/Classes/Business/Controllers/ProductViewController.m
@@ -9,7 +9,17 @@
 #import "ProductViewController.h"
 #import "RDVTabBarController.h"
 
-@interface ProductViewController ()
+@interface ProductViewController ()<UITableViewDelegate,UITableViewDataSource>{
+    UITableView *_typeNameOneTableView;
+    NSMutableArray *_typeNameOneArr;
+    UITableView *_typeNameTwoTableView;
+    NSMutableArray *_typeNameTwoArr;
+    UITableView *_num_unitTableView;
+    NSMutableArray *_num_unitArr;
+    UITableView *_price_unitTableView;
+    NSMutableArray *_price_unitArr;
+
+}
 
 @end
 
@@ -21,9 +31,132 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSArray *typeNameOneArray=@[@"1",@"2"];
+    NSArray *typeNameTwoArray=@[@"1",@"2",@"3"];
+    NSArray *num_unitArray=@[@"1",@"2",@"3",@"4"];
+    NSArray *price_unitArray=@[@"1",@"2",@"3",@"4",@"5"];
+    _typeNameOneArr=[[NSMutableArray alloc]initWithArray:typeNameOneArray];
+    _typeNameTwoArr=[[NSMutableArray alloc]initWithArray:typeNameTwoArray];
+    _num_unitArr=[[NSMutableArray alloc]initWithArray:num_unitArray];
+    _price_unitArr=[[NSMutableArray alloc]initWithArray:price_unitArray];
+    
     // Do any additional setup after loading the view from its nib.
+    [self creatUI];
 }
+-(void)creatUI{
+    
+    CGRect typeNameOneFrame=self.typeNameLabel1.frame;
+    _typeNameOneTableView=[[UITableView alloc]initWithFrame:CGRectMake(typeNameOneFrame.origin.x+typeNameOneFrame.size.width/2.0, typeNameOneFrame.origin.y+typeNameOneFrame.size.height,typeNameOneFrame.size.width, 200) style:UITableViewStylePlain];
+    _typeNameOneTableView.delegate=self;
+    _typeNameOneTableView.dataSource=self;
+    _typeNameOneTableView.backgroundColor=[UIColor clearColor];
+    
+    CGRect typeNameTwoFrame=self.typeNameLabel2.frame;
+    _typeNameTwoTableView=[[UITableView alloc]initWithFrame:CGRectMake(typeNameTwoFrame.origin.x+typeNameTwoFrame.size.width/2.0, typeNameTwoFrame.origin.y+typeNameTwoFrame.size.height,typeNameTwoFrame.size.width, 200) style:UITableViewStylePlain];
+    _typeNameTwoTableView.delegate=self;
+    _typeNameTwoTableView.dataSource=self;
+    _typeNameTwoTableView.backgroundColor=[UIColor clearColor];
+    
+    CGRect _num_unitFrame=self.num_unitLabel.frame;
+    _num_unitTableView=[[UITableView alloc]initWithFrame:CGRectMake(typeNameTwoFrame.origin.x, typeNameTwoFrame.origin.y+typeNameTwoFrame.size.height, 100, 200) style:UITableViewStylePlain];
+    _num_unitTableView.delegate=self;
+    _num_unitTableView.dataSource=self;
+    _num_unitTableView.backgroundColor=[UIColor clearColor];
+    
 
+
+
+}
+#pragma mark --选择产品类型
+- (IBAction)typeNameOneClicked:(id)sender {
+    static int i=0;
+    if (i==0) {
+        [self.view addSubview:_typeNameOneTableView];
+        i=1;
+    }else{
+        [_typeNameOneTableView removeFromSuperview];
+        i=0;
+    }
+}
+#pragma mark --选择产品子类型
+- (IBAction)typeNameTwoClicked:(id)sender {
+    [_typeNameOneTableView removeFromSuperview];
+    static int i=0;
+    if (i==0) {
+        [self.view addSubview:_typeNameTwoTableView];
+        i=1;
+    }else{
+        [_typeNameTwoTableView removeFromSuperview];
+        i=0;
+    }
+
+}
+#pragma mark --选择产品数量
+- (IBAction)num_unitClicked:(id)sender {
+    
+}
+#pragma mark --选择产品价格
+- (IBAction)price_unitClicked:(id)sender {
+    
+}
+#pragma mark --同意协议
+- (IBAction)agreeClicked:(id)sender {
+    
+}
+#pragma mark --发布
+- (IBAction)agreeReleseClicked:(id)sender {
+    
+}
+#pragma mark --回调函数
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (tableView==_typeNameOneTableView) {
+        return _typeNameOneArr.count;
+    }else if (tableView==_typeNameTwoTableView){
+        return _typeNameTwoArr.count;
+    }
+    return 0;
+
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (tableView==_typeNameOneTableView) {
+        UITableViewCell *cell=[_typeNameOneTableView dequeueReusableCellWithIdentifier:@"typeNameOne"];
+        if (cell==nil) {
+            cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"typeNameOne"];
+            cell.contentView.backgroundColor=[UIColor lightGrayColor];
+            cell.selectionStyle=UITableViewCellSelectionStyleNone;
+        }
+        cell.textLabel.text=_typeNameOneArr[indexPath.row];
+        return cell;
+    }else if (tableView==_typeNameTwoTableView){
+        UITableViewCell *cell=[_typeNameTwoTableView dequeueReusableCellWithIdentifier:@"typeNameTwo"];
+        if (cell==nil) {
+            cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"typeNameTwo"];
+            cell.contentView.backgroundColor=[UIColor lightGrayColor];
+            cell.selectionStyle=UITableViewCellSelectionStyleNone;
+        }
+        cell.textLabel.text=_typeNameTwoArr[indexPath.row];
+        return cell;
+    
+    }
+    return nil;
+
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (tableView==_typeNameOneTableView) {
+        self.typeNameLabel1.text=_typeNameOneArr[indexPath.row];
+        [_typeNameOneTableView removeFromSuperview];
+    }else if (tableView==_typeNameTwoTableView){
+        self.typeNameLabel2.text=_typeNameTwoArr[indexPath.row];
+        [_typeNameTwoTableView removeFromSuperview];
+    }
+    
+
+}
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [_typeNameOneTableView removeFromSuperview];
+    [_typeNameTwoTableView removeFromSuperview];
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
